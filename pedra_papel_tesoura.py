361928ea3daae7ea29f78a32a0f57264e967498d
2506roma <romariodiogo2021@gmail.com>
2026-01-26 21:27:22 -0300

    pseudocodigo

---
+++ pedra_papel_tesoura.py
@@ -0,0 +1,34 @@
+"""
+escolha 
+1 = pedra 
+2 = papel 
+3 = tesoura 
+
+entrada escolhe 
+sistema sorteia 
+
+se entrada == sistema 
+    empate
+se entrada == 1
+    se sistema == 2
+        sistema vence
+    se sistema == 3 
+        sistema perde
+
+se não se entrada == 2
+    se sistema == 1 
+        sistema perde
+    se sistema == 3
+        sistema vence 
+
+se não se entrada == 3
+    se sistema == 1 
+        sistema vence
+    se sistema == 2
+        sistema perde 
+
+se não 
+    escolha errada tente novamente !
+    repita tudo !!!!
+        
+"""
