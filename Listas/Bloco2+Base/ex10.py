05701e58361d68396d891268281e3912f1b78155
2506roma <romariodiogo2021@gmail.com>
2026-02-04 15:57:27 -0300

    top

--- Listas/Bloco2+Base/ex10.py
+++ Listas/Bloco2+Base/ex10.py
@@ -1,6 +1,10 @@
 numeros = [-12, 5, -3, 18, -7, 0, 9, -25, 4, -1, 30, -6]
 
-negativos = []
-for valor in numeros:
+indice_num = []
+for indece, valor in enumerate(numeros):
     if valor < 0:
-        negativos.append
+        indice_num.append(indece)
+
+for n in indice_num:
+    numeros[n] = 0
+print(numeros)
