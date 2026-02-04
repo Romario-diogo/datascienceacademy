269f1b7b2c0743875caca45af0983b3f5a167359
2506roma <romariodiogo2021@gmail.com>
2026-02-04 12:22:41 -0300

    interador e unpacking

---
+++ Listas/Bloco2+Base/ex06.py
@@ -0,0 +1,17 @@
+numeros = [34, 7, 128, 56, 19, 3, 92, 41, 15, 65]
+
+primeiro, *meio, ultimo = numeros
+
+print(primeiro)
+print(ultimo)
+
+
+# Usando o interador 
+
+it = iter(numeros)
+primeiro_it = next(it)
+
+for ultimo in it:
+    pass
+
+print(f"{primeiro}, {ultimo}")
