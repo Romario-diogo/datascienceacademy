ce13a24b505829f764a9be60e778346e65a85aea
2506roma <romariodiogo2021@gmail.com>
2026-02-03 22:35:33 -0300

    +1

---
+++ Listas/Bloco1_Base/ex04.py
@@ -0,0 +1,8 @@
+numeros = [34, 7, 128, 7, 56, 19, 3, 92, 41, 15, 65]
+
+cont = 0
+for i in numeros:
+    if i == 7:
+        cont += 1
+
+print(f"O numero 7 apareceu {cont} vezes na lista")
---
+++ Listas/Bloco1_Base/ex05.py
@@ -0,0 +1,6 @@
+nova_lista = []
+cont = 0
+while len(nova_lista) < 20:
+    cont += 1
+    nova_lista.append(cont)
+print(nova_lista)
