9c75b09bc0e9c78695b43d403a77575c7df0daf4
2506roma <romariodiogo2021@gmail.com>
2026-02-04 14:48:25 -0300

    +3

---
+++ Listas/Bloco2+Base/ex07.py
@@ -0,0 +1,4 @@
+numeros = [34, 7, 128, 56, 19, 3, 92, 41, 15, 65]
+meio = int(len(numeros)/2)
+numeros.insert(5, 10)
+print(numeros)
---
+++ Listas/Bloco2+Base/ex08.py
@@ -0,0 +1,5 @@
+numeros = [34, 7, 128, 56, 19, 3, 92, 41, 15, 65]
+tamanho = len(numeros) -1
+
+for i in range(tamanho, -1, -1):
+    print(numeros[i])
