ed7201e6e6b577b371bd82c4cdf1fe33d5550c30
2506roma <romariodiogo2021@gmail.com>
2026-02-03 22:18:33 -0300

    somando a lista

---
+++ Listas/Bloco1_Base/ex01.py
@@ -0,0 +1,6 @@
+numeros = [34, 7, 128, 56, 19, 3, 92, 41, 15, 65]
+total = 0
+for i in numeros:
+    total += i
+print(f"Total da lista com for {total}")
+print(f"total da lista com sum {sum(numeros)}")
