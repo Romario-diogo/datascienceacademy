293af1beb9a3dd65cd424bbc166b8f6497426b5d
2506roma <romariodiogo2021@gmail.com>
2026-02-04 15:51:55 -0300

    +1

---
+++ Listas/Bloco2+Base/ex10.py
@@ -0,0 +1,6 @@
+numeros = [-12, 5, -3, 18, -7, 0, 9, -25, 4, -1, 30, -6]
+
+negativos = []
+for valor in numeros:
+    if valor < 0:
+        negativos.append
---
+++ Listas/Bloco2+Base/teste.py
@@ -0,0 +1,4 @@
+numeros = [-12, 5, -3, 18, -7, 0, 9, -25, 4, -1, 30, -6]
+
+for i, v in enumerate(numeros):
+    print(i, v)
